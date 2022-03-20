0d91017a772e9cf5b9fd40aef6a7deb8bc59db84
tansut <tansu.turkoglu@kalitte.com.tr>
2022-03-21 00:15:11 +0300

    lazy developer

--- Kalitte.Trading.Host/Program.cs
+++ Kalitte.Trading.Host/Program.cs
@@ -21,7 +21,7 @@ public class Program
     public static OptimizerSettings AppTest()
     {
         var settings = new OptimizerSettings();
-        settings.Start = new DateTime(2022, 3, 14);
+        settings.Start = new DateTime(2022, 3, 1);
         settings.Finish = new DateTime(2022, 3, 18);
         settings.AutoClosePositions = true;
         settings.AutoClosePositions = true;
